71ced05068a547e3c17c2954eacb368c7d1ba96a
alexmitroff <alexmitroff@gmail.com>
2017-06-06 01:36:42 +0300

    Added organization micro markup scheme.org

--- footer.php
+++ footer.php
@@ -1,16 +1,34 @@
 <footer>
-    <div class="container">
-        <ul class="address">
+    <div class="container" itemscope itemtype="http://schema.org/Organization">
+        <meta itemprop="name" content="Ampliconyx Oy">
+        <link itemprop="url" href="http://www.ampliconyx.com">
+        <ul class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
             <li>Ampliconyx Oy</li>
-            <li>Lautakatonkatu 18, Tampere, Finland</li>
-            <li>Phone: +358 41 4774781</li>
-            <li>Email:  info@ampliconyx.com</li>
+            <li><span itemprop="streetAddress">Lautakatonkatu 18</span>, <span itemprop="addressLocality">Tampere</span>, <span itemprop="addressCountry">Finland</span></li>
+            <li>Phone: <span itemprop="telephone">+358 41 4774781</span></li>
+            <li>Email:  <span itemprop="email">info@ampliconyx.com</span></li>
         </ul>
     </div>
     <div class="copyright">
         Copyright © Ampliconyx Oy 
     </div>
 </footer>
+        <script type="application/ld+json">
+        {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "name": "Ampliconyx Oy",
+            "url": "http://ampliconyx.com",
+            "telephone": "+358-41-4774781",
+            "email": "info@ampliconyx.com",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Lautakatonkatu 18",
+                "addressLocality": "Tampere",
+                "addressCountry": "Finland"
+            }
+        }
+        </script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
         <script type="text/javascript">
